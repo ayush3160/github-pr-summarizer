412c6728eaea413cce5105d64e3fb512715569cf
ayush3160 <kshitij3160@gmail.com>
2024-08-08 20:21:35 +0530

    initial commit

---
+++ app.ts
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express();
+
+app.get("/summarise",(req,res) => {
+    console.log(req.body)
+    res.send("Hello World")
+})
+
+app.listen(3000,() => {
+    console.log("Server is listening on port 3000")
+})
