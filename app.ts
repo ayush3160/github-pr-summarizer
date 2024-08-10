14024be8b19173aad9efface75905ab2195d0711
ayush3160 <kshitij3160@gmail.com>
2024-08-10 16:33:47 +0530

    added summarise webhook

--- app.ts
+++ app.ts
@@ -1,10 +1,39 @@
-import express from "express"
+import express , {Express , Request , Response} from "express"
+import { Octokit } from "@octokit/rest";
 
-const app = express();
+const app : Express = express();
 
-app.get("/summarise",(req,res) => {
-    console.log(req.body)
-    res.send("Hello World")
+const octokit = new Octokit({
+    auth : process.env.GITHUB_TOKEN,
+    userAgent : "Pr-Summarise",
+})
+
+app.use(express.json());
+
+app.post("/summarise", async (req : Request,res : Response) => {
+    const event = req.headers["X-Github-Event"];
+
+    if(event !== "pull_request"){
+        res.status(200);
+    }
+
+    const action : string = req.body.action;
+    const draft : boolean = req.body.pull_request.draft;
+    const owner : string = req.body.repository.owner.login;
+    const repo : string = req.body.repository.name;
+    const pull_number : number = req.body.number;
+
+    if(action === "opened" && !draft){
+        const commits = await octokit.rest.pulls.listCommits({
+            owner,
+            repo,
+            pull_number
+        })
+
+        console.log(commits)
+    }
+
+    res.status(200);
 })
 
 app.listen(3000,() => {
